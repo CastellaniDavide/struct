b31ac864059a840e535e16623b194c12a375b78d
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-10 09:17:10 +0200

    Created bin/template.c

---
+++ bin/struct.c
@@ -0,0 +1,22 @@
+/**
+ * @file struct.cpp
+ *
+ * @version 01.01 2020-08-15
+ *
+ * @brief Test struct in c
+ *
+ * @ingroup struct
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+#include <stdio.h>
+
+int main() {
+	// printf() displays the string inside quotation
+	printf("struct");
+	return 0;
+}
